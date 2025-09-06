fede94fb41257813a35d7f6a37faeec7b063377f
ghost0r10n <rionhacker@gmail.com>
2025-09-06 16:07:53 +0200

    feat: concluded fade-away-text

--- src/refresh-syntax/fade-away-text.c
+++ src/refresh-syntax/fade-away-text.c
@@ -4,8 +4,10 @@
 #include <string.h>
 
 int main(){
-printf(CLEAR_TERMINAL);
-printf(HIDE_CURSOR);
+  const char* grayShade[] = GRAY_SCALE_ORD;
+
+  printf(CLEAR_TERMINAL);
+  printf(HIDE_CURSOR);
   char *banners[] = {
     "I have to pee now",
     "AHHHHHHHHHHHHHHHHHHHH",
@@ -22,56 +24,32 @@ printf(HIDE_CURSOR);
       max=bannerSize;
     }
     int counter = 0;
-    char l_char;
-    char g_char;
-    char d_char;
-    char f_char;
-    char t_char;
-    char r_char;
+    char nCharArr[FADE_SIZE] = {0};
     for(char *p=banner; *p!='\0'; p++){
-      if (counter+1<bannerSize){
 
-
+        //-------- SHADE THE BITCH --------
         usleep(SLEEP_TIME);
-        if(counter>=6)
-          printf("\033[1;%dH%s%c",counter-5,VERY_DARK_GRAY, r_char);
-        if(counter>=5)
-          printf("\033[1;%dH%s%c",counter-4,VERY_DARK_GRAY, t_char);
-        if(counter>=4)
-          printf("\033[1;%dH%s%c",counter-3,DARKER_GRAY, f_char);
-        if(counter>=3)
-          printf("\033[1;%dH%s%c",counter-2,DARK_GRAY, d_char);
-        if(counter>=2)
-          printf("\033[1;%dH%s%c",counter-1,MEDIUM_GRAY, g_char);
-        if (counter>=1)
-          printf("\033[1;%dH%s%c",counter,LIGHT_GRAY, l_char);
-        printf("\033[1;%dH%s%c",counter+1,WHITE_COLOR, *p);
-        
-        r_char = t_char;
-        t_char = f_char;
-        f_char = d_char;
-        d_char = g_char;
-        g_char = l_char;
-        l_char = *p;
-
-      }else{
-        printf("%c",*p);
-        for(int i=max-bannerSize; i!=0; i--){
-          printf("%s", RESET_COLOR);
-          max = 0;
+        for(int i = 0; i < FADE_SIZE && counter>=i+1; i++){
+          printf("\033[1;%dH%s%c",counter-i, grayShade[i], nCharArr[i]);
         }
-      }
+        //---------------------------------
 
-      
+        printf("\033[1;%dH%s%c",counter+1,WHITE_COLOR, *p);
+
+        //NOTE: Shifting right array logic
+        for(int i=FADE_SIZE-1; i>0; i--){
+          nCharArr[i] = nCharArr[i-1];
+        }
+        
+        nCharArr[0] = *p;
+      //NOTE: Flush the printf or it will buffer
       fflush(stdout);
       counter++;
-
     }
 
-    //printf("%s\033[1;%dH%c", RESET_COLOR,counter,last_char);
     printf("%s", CLEAR_TERMINAL);
   }
 
-printf(SHOW_CURSOR);
-return 0;
+  printf(SHOW_CURSOR);
+  return 0;
 }
